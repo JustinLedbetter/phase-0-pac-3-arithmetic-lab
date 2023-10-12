b4c7982c4f04242e7a52e2cdbbfd66c8c04e46a2
forithershit123@yahoo.com <forithershit123@yahoo.com>
2023-10-12 18:14:14 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b) {
+    return a + b;
+  }
+
+function subtract(a, b) {
+   return a - b
+  }
+
+function multiply(a, b) {
+   return a * b
+  }
+
+function divide(a, b) {
+   return a / b 
+  }
+
+  function increment(n) {
+    return n + 1;
+  }
+  
+  let num = 5;
+  let result = increment(num);
+  console.log(result);
+
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  var nm = 5;
+  var res = decrement(nm);
+  console.log(res);
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  console.log(makeInt("71"));
+
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+
+  console.log(preserveDecimal("6.938387")); 
+
+  
+
